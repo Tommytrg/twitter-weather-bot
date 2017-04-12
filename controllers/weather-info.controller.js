1db8f5d68b1b6b9456b3fd514638ee313f342549
Tomas Ruiz <tomastrg22@gmail.com>
2017-04-12 21:10:51 +0200

    weather info object created

--- controllers/weather-info.controller.js
+++ controllers/weather-info.controller.js
@@ -1,15 +1,25 @@
 /*jshint esversion:6*/
 const request = require('request');
-const weather = '../controllers/weather-info.controller';
-console.log('hola')
+const weatherInfoModel = require('../models/weatherInfo.model');
 
 const getWeatherInfo = (error, response, body) => {
-console.log('hola')
   if(error){
     throw new Error(error);
   }else{
-    console.log('body',body);
+    saveWeatherInfo(new weatherInfoModel({
+      coords: [JSON.parse(body).coord.lon,JSON.parse(body).coord.lat],
+      main_weather: JSON.parse(body).weather[0].main,
+      weather_description: JSON.parse(body).weather[0].description,
+      temperature: JSON.parse(body).main.temp,
+      temperature_min: JSON.parse(body).main.temp_min,
+      temperature_max: JSON.parse(body).main.temp_max
+    })
+    );
   }
+};
+
+const saveWeatherInfo= (data) => {
+  console.log(data);
 };
 
 const getWeather = request('http://api.openweathermap.org/data/2.5/weather?id=3117735&APPID=aa04432a0366e8e6600f565d1d1d6c41', getWeatherInfo);
